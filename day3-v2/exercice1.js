602e828b29c0c42727ba6af237f354ecda620272
Guillermo David <gdavid.ptorrez@gmail.com>
2019-12-19 10:35:18 -0400

    IMP: Add rxjs

---
+++ day3-v2/exercice1.js
@@ -0,0 +1,58 @@
+const Rxjs = require('rxjs');
+const Operators = require('rxjs/operators')
+const { pipe } = Rxjs;
+const { concatMap, tap, map, reduce } = Operators
+
+
+const myName = 'Guillermo David';
+
+const isVowelRegEx = (value) =>
+{
+  if (value.length == 1)
+  {
+    return /[aeiou]/.test(value);
+  }
+}
+
+
+const objSeparateString = {
+    vowals: [],
+    consonants: []
+}
+
+const separateName$ = Rxjs.of(myName.split('')).pipe(
+    tap(resTap => console.log('resTap', resTap)),
+    map(res => {
+        console.log('res', res);
+        return res;
+    }),
+    concatMap(groups => {
+        // console.log('groups', groups);
+        return groups;
+    }),
+    reduce((accumulate, current) => {
+        if (isVowelRegEx(current)) {    
+            accumulate.vowals = [...accumulate.vowals, [current]]
+        } else {
+            accumulate.consonants = [...accumulate.consonants, [current]]
+        }
+        return accumulate;
+    }, {
+        vowals: [],
+        consonants: []
+    })
+)
+
+
+
+separateName$.subscribe(response => {
+    console.log('response', response);
+
+
+    
+});
+
+
+
+
+

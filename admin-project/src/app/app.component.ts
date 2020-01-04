71158e44819835deef1c2cea703491a5490664b2
Guillermo David <gdavid.ptorrez@gmail.com>
2020-01-04 16:04:13 -0400

    IMP: Add admin projects
    - Angular framekork
    - Bootstrap 4
    -

--- admin-project/src/app/app.component.ts
+++ admin-project/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'admin-project';
+
+  constructor(private loadingService: NgxSpinnerService) {
+
+  }
+
+  ngOnInit() {
+    console.log('asdasdasdasdasd');
+    this.loadingService.show();
+
+    setTimeout(() => this.loadingService.hide(), 10000);
+  }
+
 }
--- admin-project/src/app/app.module.ts
+++ admin-project/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxSpinnerModule } from 'ngx-spinner';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserComponent } from './components/user/user.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
